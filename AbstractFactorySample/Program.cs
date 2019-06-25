dea25702cc1473934012d289afe89d5f83cc2126
liupenghao <haochen214@163.com>
2019-06-25 21:55:42 +0800

    配置文件和客户端测试类

--- AbstractFactorySample/Program.cs
+++ AbstractFactorySample/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
 
 namespace AbstractFactorySample
 {
@@ -10,6 +12,27 @@ namespace AbstractFactorySample
     {
         static void Main(string[] args)
         {
+            // 使用抽象层定义
+            SkinFactory factory;
+            Button bt;
+            TextField tf;
+            ComboBox cb;
+
+            // 读取配置文件
+            string factoryType = ConfigurationManager.AppSettings["factory"];
+
+            // 反射生成对象
+            factory = (SkinFactory)Assembly.Load("AbstractFactorySample").CreateInstance(factoryType);
+
+            bt = factory.CreateButton();
+            tf = factory.CreateTextField();
+            cb = factory.CreateComboBox();
+
+            bt.Display();
+            tf.Display();
+            cb.Display();
+
+            Console.ReadLine();
         }
     }
 }
